2604fae3f01160c5346d6b0d1735aeb6ff5b4f7a
Retesh <retesh.bajaj@gmail.com>
2015-08-11 10:50:10 +0100

    Day 2 First commit

--- app/server.rb
+++ app/server.rb
@@ -5,6 +5,21 @@ class BookmarkManager < Sinatra::Base
     'Hello BookmarkManager!'
   end
 
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
+  end
+
+  get '/links/new' do
+    erb :'links/new'
+  end
+
+  post '/links' do
+    Link.create(url: params[:url], title: params[:title])
+    redirect to('/links')
+  end
+
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -3,8 +3,13 @@ require 'spec_helper'
 
 feature 'Viewing links' do
 
+  scenario "it can visit the home page" do
+    visit '/'
+      expect(page).to have_content('Hello')
+  end
+
   scenario 'the hompage has the link to the database' do
-    Link.new(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
     visit '/links'
     expect(page.status_code).to eq 200
     within 'ul#links' do
